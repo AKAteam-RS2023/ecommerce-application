ae75c06ecd301fd7c8c5835d300a0df71cc8f9d3
Alena <alenzija@gmail.com>
2023-08-04 12:11:18 +0200

    ECA-4 init: add jest

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import './style.scss';
 
 import svg from './assets/image/rs_school_js.svg';
 
-const a = 5;
-
-console.log(a);
 const img = document.createElement('img');
 
 img.src = svg;
 
 document.body.append(img);
+
+export const sum = (a: number, b: number): number => a + b;
