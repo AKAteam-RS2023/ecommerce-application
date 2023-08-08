0f71f31fcded47d63f30f59535e6fcbd485fa677
Alena <alenzija@gmail.com>
2023-08-08 21:42:49 +0200

    ECA-9 init: change project's structure and install normalize.css

---
+++ src/components/app/index.ts
@@ -0,0 +1,7 @@
+import './app.scss';
+
+export default class App {
+  public static start(): void {
+    console.log('start app');
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import './style.scss';
+import App from './components/app';
 
-import svg from './assets/image/rs_school_js.svg';
-
-const img = document.createElement('img');
-
-img.src = svg;
-
-document.body.append(img);
-
-export const sum = (a: number, b: number): number => a + b;
+App.start();
