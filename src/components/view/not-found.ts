6010edbae29f7105b5fb7b02480de846821083ec
alinatsydzik <pronevich.alina@gmail.com>
2023-08-19 14:27:57 +0200

    feat: add basic styles and image for 404 page

--- src/components/view/not-found.ts
+++ src/components/view/not-found.ts
@@ -1,5 +1,7 @@
 import createElement from '../../dom-helper/create-element';
 import { IPage } from '../../types/interfaces/page';
+import notFoundImg from '../../assets/image/404-page-not-found.png';
+import '../../styles/pages/not-found.scss';
 
 export class NotFound implements IPage {
   private containerPage?: HTMLDivElement;
@@ -12,13 +14,18 @@ export class NotFound implements IPage {
     this.containerPage = createElement<HTMLDivElement>('div', {
       class: 'not-found',
     });
+    const pageImage = createElement<HTMLImageElement>('img', {
+      class: 'not-found-img',
+    });
+    pageImage.src = notFoundImg;
+    pageImage.alt = 'Page not found';
     this.pageHeader = createElement<HTMLElement>('h1');
     this.pageHeader.innerText = 'Page not found';
     this.pageText = createElement<HTMLDivElement>('div', {
       class: 'page-text',
     });
     this.pageText.innerHTML = '<p>Please choose another page from the menu or go to the <a href="/">home page</a>.</p>';
-    this.containerPage.append(this.pageHeader, this.pageText);
+    this.containerPage.append(pageImage, this.pageHeader, this.pageText);
     return this.containerPage;
   }
 }
