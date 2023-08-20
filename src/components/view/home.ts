72ff073ec86855769981b1abce06af5103e25a20
alinatsydzik <pronevich.alina@gmail.com>
2023-08-20 19:58:57 +0200

    feat: add links to main page

--- src/components/view/home.ts
+++ src/components/view/home.ts
@@ -2,12 +2,34 @@ import createElement from '../../dom-helper/create-element';
 import { IPage } from '../../types/interfaces/page';
 
 export class Home implements IPage {
-  private homePageWrapper?: HTMLDivElement;
+  private linksWrapper?: HTMLDivElement;
+
+  private hasUser = !!localStorage.getItem('userToken');
 
   public render(): HTMLDivElement {
-    this.homePageWrapper = createElement('div', {
-      class: 'home-page__wrapper',
+    this.linksWrapper = createElement('div', {
+      class: 'home-links__wrapper',
     });
-    return this.homePageWrapper;
+
+    const homeLink = createElement('a', {
+      class: 'home-links__item link--home',
+      href: '/',
+    });
+    homeLink.innerText = 'Home';
+
+    const loginLink = createElement('a', {
+      class: 'home-links__item link--login',
+      href: this.hasUser ? '/' : '/login',
+    });
+    loginLink.innerText = 'Login';
+
+    const registrationLink = createElement('a', {
+      class: 'home-links__item link--registration',
+      href: '/registration',
+    });
+    registrationLink.innerText = 'Registration';
+
+    this.linksWrapper.append(homeLink, registrationLink, loginLink);
+    return this.linksWrapper;
   }
 }
