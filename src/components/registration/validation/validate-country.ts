c7681349bdf168b4af204b6cb529c7b8208630de
shep-js <kateshakurina@gmail.com>
2023-08-21 21:56:37 +0200

    fix: registration styles

--- src/components/registration/validation/validate-country.ts
+++ src/components/registration/validation/validate-country.ts
@@ -2,6 +2,6 @@ import { Country } from '../country';
 
 export default function validateCountry(country: string): void {
   if (country !== Country.Poland && country !== Country.Lithuania) {
-    throw new Error(`Unfortunately we deliver only to ${Country.Poland} and ${Country.Lithuania}`);
+    throw new Error(`We deliver only to ${Country.Poland} and ${Country.Lithuania}`);
   }
 }
