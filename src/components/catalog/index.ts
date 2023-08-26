c8ce09b27a845403ffbb3ef6bc8c739120e5b404
Alena <alenzija@gmail.com>
2023-08-26 11:15:50 +0200

    ECA-34 feat: create class Product

--- src/components/catalog/index.ts
+++ src/components/catalog/index.ts
@@ -1,10 +1,19 @@
 import createElement from '../../dom-helper/create-element';
+import Product from '../product';
 import './catalog.scss';
+
+import chair from '../../assets/image/Images.png';
 
 export default class Catalog {
   private container = createElement('div', { class: 'catalog' });
 
+  private init(): void {
+    const product = new Product(chair, 'Leviosa', 'Stylish cafe chair', 2500);
+    this.container.append(product.render());
+  }
+
   public render(): HTMLElement {
+    this.init();
     return this.container;
   }
 }
--- src/components/product/index.ts
+++ src/components/product/index.ts
@@ -1,1 +1,48 @@
+import createElement from '../../dom-helper/create-element';
 import './product.scss';
+
+export default class Product {
+  private product = createElement('article', { class: 'product' });
+
+  constructor(
+    public imageSrc: string,
+    public name: string,
+    public description: string,
+    public price: number,
+  ) {
+    this.imageSrc = imageSrc;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
+
+  private init(): void {
+    const img = createElement<HTMLImageElement>('img', {
+      class: 'product__img',
+      src: this.imageSrc,
+      alt: "product's photo",
+    });
+    const name = createElement('div', {
+      class: 'product__title',
+    });
+    name.textContent = this.name;
+    const description = createElement('div', {
+      class: 'product__description',
+    });
+    description.textContent = this.description;
+    const price = createElement('div', {
+      class: 'product__price',
+    });
+    price.textContent = `${this.price}`;
+    const wrapper = createElement('div', {
+      class: 'product__wrapper',
+    });
+    wrapper.append(name, description, price);
+    this.product.append(img, wrapper);
+  }
+
+  public render(): HTMLElement {
+    this.init();
+    return this.product;
+  }
+}
