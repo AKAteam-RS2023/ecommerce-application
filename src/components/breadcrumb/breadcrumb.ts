4bbef4afba75b65e747d004cfb8a351a147a3f21
Alena <alenzija@gmail.com>
2023-08-29 18:05:20 +0200

    ECA-45 feat: implement breadcrumb

--- src/components/breadcrumb/breadcrumb.ts
+++ src/components/breadcrumb/breadcrumb.ts
@@ -1,5 +1,8 @@
 import createElement from '../../dom-helper/create-element';
 import eventEmitter from '../../dom-helper/event-emitter';
+
+import Crumb from '../crumb';
+
 import { getCategoryById } from '../../services/ecommerce-api';
 
 import './breadcrumb.scss';
@@ -9,42 +12,61 @@ const LANGUAGE = 'pl-PL';
 export default class BreadCrumb {
   private container = createElement('div', { class: 'breadcrumb' });
 
-  private firstCrumb = createElement('a', { href: '#!', class: 'breadcrumb__item' });
+  private firstCrumb = createElement('div', { class: 'crumb' });
+
+  private selectCategory: string | null = null;
 
   constructor() {
+    this.initFirstCrumb();
     this.init();
-    eventEmitter.subscribe('event: change-category', (data) => {
-      if (!data || !('id' in data)) {
-        return;
-      }
-      this.container.innerHTML = '';
-      this.container.append(this.firstCrumb);
-      if ('parentId' in data) {
-        getCategoryById(data.parentId)
-          .then((res) => {
-            const crumb = createElement('a', { href: '#!', class: 'breadcrumb__item' });
-            crumb.textContent = `${res.name[LANGUAGE]}> `;
-            this.container.append(crumb);
-          })
-          .then(() => {
-            getCategoryById(data.id).then((subcategory) => {
-              const crumb = createElement('a', { href: '#!', class: 'breadcrumb__item' });
-              crumb.textContent = subcategory.name[LANGUAGE];
-              this.container.append(crumb);
+    eventEmitter.subscribe('event: change-category', (data) => this.changeCategory(data));
+  }
+
+  private changeCategory = (data?: Record<string, string>): void => {
+    if (!data || !('id' in data)) {
+      return;
+    }
+    if (this.selectCategory && data.id === this.selectCategory) {
+      return;
+    }
+    this.container.innerHTML = '';
+    this.selectCategory = data.id;
+    this.container.append(this.firstCrumb);
+    if ('parentId' in data) {
+      getCategoryById(data.parentId)
+        .then((res) => {
+          const crumb = new Crumb({ id: data.parentId, name: res.name[LANGUAGE] });
+          this.container.append(crumb.render());
+        })
+        .then(() => {
+          getCategoryById(data.id).then((subcategory) => {
+            const crumb = new Crumb({
+              id: data.id,
+              parentId: data.parentId,
+              name: subcategory.name[LANGUAGE],
             });
+            this.container.append(crumb.render());
           });
-      } else {
-        getCategoryById(data.id).then((subcategory) => {
-          const crumb = createElement('a', { href: '#!', class: 'breadcrumb__item' });
-          crumb.textContent = `${subcategory.name[LANGUAGE]}> `;
-          this.container.append(crumb);
         });
-      }
-    });
+    } else {
+      getCategoryById(data.id).then((subcategory) => {
+        const crumb = new Crumb({ id: data.id, name: subcategory.name[LANGUAGE] });
+        this.container.append(crumb.render());
+      });
+    }
+  };
+
+  private initFirstCrumb(): void {
+    this.firstCrumb.textContent = 'All products> ';
+    this.firstCrumb.onclick = (): void => {
+      this.init();
+      eventEmitter.emit('event: show-all-products', undefined);
+    };
   }
 
   private init(): void {
-    this.firstCrumb.textContent = 'All products> ';
+    this.selectCategory = null;
+    this.container.innerHTML = '';
     this.container.append(this.firstCrumb);
   }
 
--- src/components/catalog/index.ts
+++ src/components/catalog/index.ts
@@ -19,6 +19,8 @@ export default class Catalog {
 
   private breadcrumb = new BreadCrumb();
 
+  private selectCategory: string | null = null;
+
   constructor() {
     this.init();
     eventEmitter.subscribe('event: change-category', (data) => {
@@ -27,11 +29,18 @@ export default class Catalog {
       }
       this.initByCategory(data.id);
     });
+    eventEmitter.subscribe('event: show-all-products', () => {
+      if (!this.selectCategory) {
+        return;
+      }
+      this.init();
+    });
   }
 
   private init(): void {
     this.container.innerHTML = '';
     this.products = [];
+    this.selectCategory = null;
     getAllProducts()
       .then((productsResponse) => {
         this.products = productsResponse.map((product) => new ProductCard(product));
@@ -43,12 +52,16 @@ export default class Catalog {
   }
 
   private initByCategory(id: string): void {
+    if (this.selectCategory && id === this.selectCategory) {
+      return;
+    }
     this.container.innerHTML = '';
     this.products = [];
     getProductsbyCategory(id)
       .then((productsResponse) => {
         this.products = productsResponse.map((product) => new ProductCard(product));
         this.products.forEach((product) => this.container.append(product.render()));
+        this.selectCategory = id;
       })
       .catch((err) => {
         this.container.textContent = err.message;
