b12346ca919fe34d41466c651878fbdea630d022
Alena <alenzija@gmail.com>
2023-09-01 10:50:56 +0200

    ECA-39 fix: fix doProduct function

--- src/controller/get-products.ts
+++ src/controller/get-products.ts
@@ -4,6 +4,7 @@ import {
   ProductProjection,
   ProductVariant,
 } from '@commercetools/platform-sdk';
+
 import IProduct from '../types/product';
 import { Sort } from '../types/sort';
 
@@ -11,60 +12,48 @@ import { getProducts } from '../services/ecommerce-api';
 
 const LANGUAGE = 'pl-PL';
 
-const getName = (data: ProductData | ProductProjection): string => data.name[LANGUAGE];
+const getName = (data: ProductData | ProductProjection): string => (data.name[LANGUAGE] ? data.name[LANGUAGE] : 'no name');
 
-const getDescription = (data: ProductData | ProductProjection): string => (data.description ? data.description[LANGUAGE] : 'No description');
+const getDescription = (data: ProductData | ProductProjection): string => (data.description && data.description[LANGUAGE] ? data.description[LANGUAGE] : 'No description');
 
-const getUrl = (data: ProductData | ProductVariant | ProductProjection): string => {
-  if ('masterVariant' in data) {
-    return data.masterVariant.images
-      ? data.masterVariant.images[0].url
-      : '../assets/image/image-not-found.png';
+const getUrl = (data: ProductData | ProductVariant | ProductProjection): string | undefined => {
+  const newData = 'masterVariant' in data ? data.masterVariant : data;
+  if (!newData.images || !Array.isArray(newData.images) || newData.images.length === 0) {
+    return undefined;
   }
-  return data.images ? data.images[0].url : '../assets/image/image-not-found.png';
+  return newData.images[0].url ? newData.images[0].url : undefined;
 };
 
 const getPrice = (data: ProductData | ProductVariant | ProductProjection): string => {
-  if ('masterVariant' in data) {
-    return data.masterVariant.prices
-      ? `${data.masterVariant.prices[0].value.centAmount / 100} ${
-        data.masterVariant.prices[0].value.currencyCode
-      }`
-      : 'No price';
+  const newData = 'masterVariant' in data ? data.masterVariant : data;
+  if (!newData.prices || !Array.isArray(newData.prices) || newData.prices.length === 0) {
+    return 'no price';
   }
-  return data.prices
-    ? `${data.prices[0].value.centAmount / 100} ${data.prices[0].value.currencyCode}`
+  return newData.prices[0].value.centAmount && newData.prices[0].value.currencyCode
+    ? `${(newData.prices[0].value.centAmount / 100).toFixed(2)} ${
+      newData.prices[0].value.currencyCode
+    }`
     : 'no prices';
 };
 
 const getDiscount = (
   data: ProductData | ProductVariant | ProductProjection,
 ): { id?: string; value?: string } | undefined => {
-  if ('masterVariant' in data) {
-    if (!data.masterVariant.prices) {
-      return undefined;
-    }
-    const value = data.masterVariant.prices[0]?.discounted?.value.centAmount;
-    return data.masterVariant.prices
-      && data.masterVariant.prices[0]?.discounted
-      && !Number.isNaN(data.masterVariant.prices[0]?.discounted?.value.centAmount)
-      ? {
-        id: data.masterVariant.prices[0]?.discounted.discount.id,
-        value: value
-          ? `${value / 100} ${data.masterVariant.prices[0]?.discounted?.value.currencyCode}`
-          : undefined,
-      }
-      : undefined;
-  }
-  if (!data.prices) {
+  const newData = 'masterVariant' in data ? data.masterVariant : data;
+  if (!newData.prices || !Array.isArray(newData.prices) || newData.prices.length === 0) {
     return undefined;
   }
-  const value = data.prices[0]?.discounted?.value.centAmount;
-  return data.prices && data.prices[0]?.discounted
+  if (!newData.prices[0].discounted) {
+    return undefined;
+  }
+  const value = newData.prices[0]?.discounted?.value.centAmount;
+  return value
+    && newData.prices[0].discounted.discount.id
+    && newData.prices[0].discounted.value.currencyCode
     ? {
-      id: data.prices[0]?.discounted?.discount.id,
+      id: newData.prices[0]?.discounted?.discount.id,
       value: value
-        ? `${value / 100} ${data.prices[0]?.discounted?.value.currencyCode}`
+        ? `${(value / 100).toFixed(2)} ${newData.prices[0]?.discounted?.value.currencyCode}`
         : undefined,
     }
     : undefined;
@@ -81,7 +70,7 @@ function doProduct(product: Product | ProductProjection): IProduct[] {
     price: getPrice(item),
     discount: getDiscount(item),
   });
-  if (item.variants.length > 0) {
+  if (item.variants && item.variants.length > 0) {
     item.variants.forEach((variant) => {
       result.push({
         id: product.id,
